afa202901ab070b4d71a2a8914e0442f17ba62cb
leedaye0412 <neoldy00@naver.com>
2025-10-24 14:10:23 +0900

    fix: 이미지 최적화 비활성화

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true, //이미지 최적화 비활성화
+  },
 };
 
 const withNextIntl = createNextIntlPlugin();
